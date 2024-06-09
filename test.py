3d7a761f20023d0740a4dfffc2f64186c2ec3604
Ryan Craig <rycraig@outlook.com>
2024-06-09 16:56:12 +1200

    Change.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 """Module providing a function printing python version."""
+import os
+OpenAPIKey = os.environ.get('OPENAI_API_KEY')
 
 def print_world():
     """Function printing python version."""
@@ -8,5 +10,3 @@ if __name__ == "__main__":
     def main():
      """Main function."""
     print_world()
-
-
