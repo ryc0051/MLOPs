75695a0f7c0d4b7277ddcf9eec3d6e5eecd205ec
Ryan Craig <rycraig@outlook.com>
2024-06-09 17:19:57 +1200

    ,...

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 """Module providing a function printing python version."""
+from langchain_openai import OpenAI
 import os
 OpenAPIKey = os.environ.get('OPENAI_API_KEY')
 
 def print_world():
     """Function printing python version."""
-    print("Hello, World!")
+    llm = OpenAI(model_name="text-ada-001", openai_api_key=OpenAPIKey)
+    print(llm("Tell me a joke about data scientist"))
 
 if __name__ == "__main__":
     def main():
