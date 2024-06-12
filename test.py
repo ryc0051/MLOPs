268f2e8b9f3b42287a460d8d920ae188056d18a2
Ryan Craig <rycraig@outlook.com>
2024-06-12 18:54:44 +1200

    ??

--- test.py
+++ test.py
@@ -1,23 +1,38 @@
 """Module providing a function printing python version."""
 import os
-from langchain.chains import create_sql_query_chain
-from langchain_community.utilities import SQLDatabase
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
 OpenAPIKey = os.environ.get("OPENAI_API_KEY")
 
 
 def print_world():
     """Function printing python version."""
-db = SQLDatabase.from_uri("sqlite:///Chinook.db")
-print(db.dialect)
-print(db.get_usable_table_names())
-db.run("SELECT * FROM Artist LIMIT 10;")
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant that translates {input_language} to {output_language}.",
+        ),
+        ("human", "{input}"),
+    ]
+)
 
-llm = ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=  OpenAPIKey,temperature=0)
-chain = create_sql_query_chain(llm, db)
-response = chain.invoke({"question": "How many employees are there"})
-print(response)
+chain = prompt | llm
+chain.invoke(
+    {
+        "input_language": "English",
+        "output_language": "German",
+        "input": "I love programming.",
+    }
+)
 
 if __name__ == "__main__":
     def main():
